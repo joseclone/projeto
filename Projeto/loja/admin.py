5b98d9e5e8e1d17ce1ffb64a4fbc5a95a7e2af7d
joseclone <jonshonzaq23@gmail.com>
2024-07-26 14:14:45 -0300

    Update admin.py

--- Projeto/loja/admin.py
+++ Projeto/loja/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+# class Produtos(admin.ModelAdmin):
+#      list_display = ("nomesDosModels")
 
 admin.site.register(Produto)
